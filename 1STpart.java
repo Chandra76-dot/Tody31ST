9b10378eff23e223c13d0e1c0c405bd379c6939b
Chandra76-dot <61492749+Chandra76-dot@users.noreply.github.com>
2020-03-31 03:02:53 -0700

    Create 1STpart.java

---
+++ 1STpart.java
@@ -0,0 +1,10 @@
+public class Customer {
+	private int customerid;
+	private String customername;
+	private String customeraddress;
+	private long creditcardnumber;
+	private long mobilenumber;
+	private String productname;
+	private double price;
+	private int quantity;
+  }
